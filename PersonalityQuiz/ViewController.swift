49491ece20383fe489db518ad00c14e2d0998521
yegorheiz <yegorheiz@gmail.com>
2020-07-27 23:45:36 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Yegor Heiz on 27.07.2020.
+//  Copyright © 2020 Yegor Heiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
